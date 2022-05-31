ccfdb8552ff0e463ba467180732c7175bdc7e102
kizzanaome <naomekizzan@gmail.com>
2022-05-31 20:38:57 +0300

    bg(fix tests)- Fix failing app test

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -2,8 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../app';
 
+
+
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  const TestComponet = () => {
+    const genreOptions = React.useContext(FetcherContext)
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  }
+
+
 });
